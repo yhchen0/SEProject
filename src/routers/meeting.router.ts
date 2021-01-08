170ea0f4b1c6740449f655253a35b81956b551fb
okesseko <59650459+okesseko@users.noreply.github.com>
2021-01-08 23:44:12 +0800

    TZ fixed

--- src/routers/meeting.router.ts
+++ src/routers/meeting.router.ts
@@ -13,6 +13,7 @@ router.get('/meeting', async (req, res) => {
         /* Parse input day 'YYYY-MM-DD', then search meeting info in database. */
         const meetgins = await client.meetings.findMany({
             where: {
+                /* Time Zone Fix */
                 beginAt:  { gte: moment(Date.parse(`${day}T00:00:00.000Z`) - 3600 * 1000 * 8).toJSON() },
                 finishAt: { lte: moment(Date.parse(`${day}T17:59:59.000Z`)).toJSON() }
             },
