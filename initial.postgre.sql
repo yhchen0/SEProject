a3bc765208f444681e0e5e0c48cc460fb26592f2
yhc <thc>
2021-01-08 15:34:08 +0800

    資料

--- initial.postgre.sql
+++ initial.postgre.sql
@@ -1,24 +1,27 @@
-CREATE TYPE Room as ENUM('ROOM1','ROOM2','ROOM3','ROOM4','ROOM5','ROOM6');
+CREATE TYPE "ROOM" as ENUM('ROOM1','ROOM2','ROOM3','ROOM4','ROOM5','ROOM6');
 
-CREATE TABLE Users(
-    id               SERIAL NOT NULL PRIMARY KEY,
-    user_name       VARCHAR NOT NULL,
-    user_token      VARCHAR NOT NULL,
-    created_at    TIMESTAMP NOT NULL DEFAULT(NOW()),
-    updated_at    TIMESTAMP NOT NULL DEFAULT(NOW())
+CREATE TABLE "Users"(
+  "user_id"         VARCHAR NOT NULL PRIMARY KEY,
+  "user_name"       VARCHAR NOT NULL,
+  "created_at"    TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP),
+  "updated_at"    TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP)
 );
 
-CREATE TABLE Meeting(
-    id           SERIAL NOT NULL PRIMARY KEY,
-    room_id        Room NOT NULL,
-    begin_at  TIMESTAMP NOT NULL DEFAULT(NOW()),
-    finish_at TIMESTAMP NOT NULL DEFAULT(NOW())
+CREATE TABLE "Meetings"(
+  "id"             SERIAL NOT NULL PRIMARY KEY,
+  "room_id"          ROOM NOT NULL,
+  "begin_at"    TIMESTAMP NOT NULL,
+  "finish_at"   TIMESTAMP NOT NULL,
+  "information"   VARCHAR
 );
 
-CREATE TABLE Attendees(
-    id         SERIAL NOT NULL,
-    meeting_id BIGINT NOT NULL,
-    user_id    BIGINT NOT NULL,
-    FOREIGN KEY(user_id)    REFERENCES Users(id),
-    FOREIGN KEY(meeting_id) REFERENCES Meeting(id)
+/* Relation */
+CREATE TABLE "_MeetingsToUsers"(
+  "A"          BIGINT    NOT NULL,
+  "B"          VARCHAR   NOT NULL,
+  "created_at" TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP),
+  FOREIGN KEY ("A") REFERENCES "Meetings"(id),
+  FOREIGN KEY ("B") REFERENCES "Users"(user_id)
 );
+CREATE UNIQUE INDEX "_MeetingsToUsers_Pair"    ON "_MeetingsToUsers"("A" ,"B");
+CREATE        INDEX "_MeetingsToUsers_Meeting" ON "_MeetingsToUsers"("A");
