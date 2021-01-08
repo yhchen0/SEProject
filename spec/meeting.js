558d76be041f984a016fa0398d92cc664e7a9b22
yhc <thc>
2021-01-08 17:06:27 +0800

    測試資料

--- spec/meeting.js
+++ spec/meeting.js
@@ -1,38 +1,44 @@
 const { baseUrl } = require('./env');
 const fetch = require('node-fetch');
 
-/* GET */
-fetch(`${baseUrl}/api/meeting?day=YYYY-MM-DD`)
+async function main() {
+    /* GET */
+    // await fetch(`${baseUrl}/api/meeting?day=2021-01-07`)
 
-/* POST */
-fetch(`${baseUrl}/api/meeting`, {
-    method: 'POST',
-    headers: { 'Content-type': 'application/json'},
-    body: JSON.stringify({
-        "title": "Meeting 1",
-        "startTime": 1610019479984,
-        "endTime": 1610023079984,
-        "moreInformation": "NyaHello",
-        "roomId": 'ROOM1',
-        "members": ["103325557309369548271"]
-    })
-});
+    // /* POST */
+    await fetch(`${baseUrl}/api/meeting`, {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json'},
+        body: JSON.stringify({
+            "title": "Meeting 1",
+            "startTime": 1610019479984,
+            "endTime": 1610023079984,
+            "moreInformation": "NyaHelloCC",
+            "roomId": 'ROOM1',
+            "members": ["000000000000000000001"]
+        })
+    });
 
-/* PUT */
-fetch(`${baseUrl}/api/meeting/2`, {
-    method: 'put',
-    headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify({
-        "title": "Meeting 1",
-        "startTime": 1610019479984,
-        "endTime": 1610023079984,
-        "moreInformation": "ABSFBFDB",
-        "roomId": 'ROOM1',
-        "members": ["000000000000000000004", "000000000000000000005"]
-    })
-});
+    // /* PUT */
+    // await fetch(`${baseUrl}/api/meeting/1`, {
+    //     method: 'put',
+    //     headers: { 'Content-type': 'application/json' },
+    //     body: JSON.stringify({
+    //         "title": "Meeting 1",
+    //         "startTime": 1610019479984,
+    //         "endTime": 1610023079984,
+    //         "moreInformation": "ABSFBFDB",
+    //         "roomId": 'ROOM1',
+    //         "members": ["000000000000000000004", "000000000000000000005"]
+    //     })
+    // });
 
-/* UPDATE */
-fetch(`${baseUrl}/api/meeting/2`, {
-    method: 'delete',
-});
+    // /* UPDATE */
+    // await fetch(`${baseUrl}/api/meeting/1`, {
+    //     method: 'delete',
+    // });
+
+
+    process.exit();
+}
+main();
